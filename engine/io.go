3bef7dfc891a88aa6c4accf70fd00469d778a978
dahui58 <dahui58@hotmail.com>
2019-06-11 01:13:40 +0100

    Add format string version NewLinef()

--- engine/io.go
+++ engine/io.go
@@ -5,17 +5,24 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"gostories/parser"
 )
 
 func NewLine(output string) {
 	fmt.Println(output)
 }
 
+func NewLinef(output string, args ...interface{}) {
+	NewLine(fmt.Sprintf(output, args...))
+}
+
 var Reader = bufio.NewReader(os.Stdin)
 
-func SimpleParse(input string) {
+func SimpleParse(input string) (parser.Action, string) {
 	split := strings.Split(input, " ")
-	for _, s := range split {
-		NewLine(s)
-	}
+	//debug
+	NewLine("0 -> " + split[0])
+	NewLine("1 -> " + split[1])
+	return parser.ParseInput(split[0], strings.TrimRight(split[1], "\r\n"))
 }
