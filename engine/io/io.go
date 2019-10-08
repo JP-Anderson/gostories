6dfb6dc22a220aeddacffbff848717d49cd7f7ec
JP <dahui58@hotmail.com>
2019-10-08 19:45:45 +0100

    Interface for mocking and swapping IO

--- engine/io/io.go
+++ engine/io/io.go
@@ -10,6 +10,27 @@ import (
 	"gostories/parser"
 )
 
+// InputOutputHandler specifies the behaviour a component must implement in order to pass input into
+// the game engine, and receive output.
+type InputOutputHandler interface {
+
+	// NewLine outputs the provided string to the player. Or returns an error.
+	NewLine(output string) error
+
+	// NewLinef outputs the provided formatted string/args to the player. Or returns an error.
+	NewLinef(output string, args ...interface{}) error
+
+	// ReadInt returns an integer as input from the player, or an error.
+	ReadInt() (int, error)
+
+	// SimpleParse parses textual input from the player and returns an Action and a target
+	// for the action in-game. It cannot error, but can return an UnknownAction.
+	SimpleParse() (parser.Action, string)
+
+}
+
+var ActiveInputOutputHandler InputOutputHandler = nil
+
 // NewLine takes a string and prints it to the console.
 func NewLine(output string) {
 	fmt.Println(output)
