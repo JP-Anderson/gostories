844d17748fbbc13362c42f5aa8bd570cf1fc1998
JP <dahui58@hotmail.com>
2019-10-09 22:49:40 +0100

    Refactor travel command to action package and add test

---
+++ engine/action/travel.go
@@ -0,0 +1,25 @@
+package action
+
+import (
+	"strings"
+
+	"gostories/engine/io"
+	"gostories/engine/io/console"
+	"gostories/engine/state"
+	"gostories/things/area"
+)
+
+// ExecuteTravelCommand takes a direction target string, and a game state struct. If the current
+// area of the game State has an exit matching the provided direction, the player will travel to
+// a new area via the exit. The function returns true if exit is valid and the player has travelled.
+func ExecuteTravelCommand(travelTarget string, state *state.State) bool {
+	trimmed := consoleio.Trim(strings.ToLower(travelTarget))
+	exit, exists := state.CurrentArea.Exits[area.Direction(trimmed)]
+	if exists {
+		state.CurrentArea = exit.To
+		return true
+	}
+	io.ActiveInputOutputHandler.NewLinef("Could not find an exit to the %v", trimmed)
+	return false
+}
+
