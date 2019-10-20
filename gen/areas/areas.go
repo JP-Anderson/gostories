56d832f3ba9bfa272a4d37fffad680bc7d48cfcc
JP <dahui58@hotmail.com>
2019-10-20 14:01:03 +0100

    Now loads Areas with features, beings, and exits from XML

--- gen/areas/areas.go
+++ gen/areas/areas.go
@@ -4,6 +4,7 @@ import (
 	"encoding/xml"
 
 	"gostories/engine/io"
+	"gostories/gen/features"
 	"gostories/things"
 	"gostories/things/area"
 	gxml "gostories/xml"
@@ -11,35 +12,72 @@ import (
 
 // Area returns any area which has a name matching the provided name.
 func Area(name string) area.Area {
-	return areas[name]
+	return *areas[name]
 }
 
 var areas = getAreas()
 
-func getAreas() map[string]area.Area {
+func getAreas() map[string]*area.Area {
 	return loadFromXML()
 }
 
-func loadFromXML() (items map[string]area.Area) {
+func loadFromXML() (items map[string]*area.Area) {
 	return areasFromXML(gxml.BytesForAreas())
 }
 
-func areasFromXML(xmlBytes []byte) map[string]area.Area {
+func areasFromXML(xmlBytes []byte) map[string]*area.Area {
 	t := &Areas{}
 	err := xml.Unmarshal(xmlBytes, t)
 	if err != nil {
 		print("err here")
 		io.ActiveInputOutputHandler.NewLinef("areasFromXML failed: %v", err)
 	}
-	m := make(map[string]area.Area, len(t.XArea))
+	m := make(map[string]*area.Area, len(t.XArea))
 	for _, a := range t.XArea {
-		newArea := area.Area{
+		newArea := &area.Area{
 			Look: a.LookText,
 		}
 		if len(a.Beings) > 0 {
-
+			//hardcoding the only being for now. TODO generate with XML
+			newArea.Beings = []*things.Being{
+				things.NewBubbles(),
+			}
+		}
+		newArea.Features = []things.Feature{}
+		for _, f := range a.Features.Feature {
+			newArea.Features = append(newArea.Features, features.Feature(f.Name))
 		}
 		m[a.Name] = newArea
+	}
+	// Loop a second time to add the exits between Areas (we need to do this after the first
+	// loop to ensure all the Areas are created when we start linking them.)
+	for _, a := range t.XArea {
+		current := m[a.Name]
+		current.Exits = make(map[area.Direction]area.Exit)
+		if a.Exits.North != "" {
+			current.Exits[area.North] = area.Exit{
+				To:   m[a.Exits.North],
+				From: m[a.Name],
+			}
+		}
+		if a.Exits.East != "" {
+			current.Exits[area.East] = area.Exit{
+				To:   m[a.Exits.East],
+				From: m[a.Name],
+			}
+		}
+		if a.Exits.South != "" {
+			current.Exits[area.South] = area.Exit{
+				To:   m[a.Exits.South],
+				From: m[a.Name],
+			}
+		}
+		if a.Exits.West != "" {
+			current.Exits[area.West] = area.Exit{
+				To:   m[a.Exits.West],
+				From: m[a.Name],
+			}
+		}
 	}
 	return m
 }
@@ -53,9 +91,20 @@ type Areas struct {
 type XArea struct {
 	Name     string
 	LookText string
-	// TODO Exits    map[string]
-	Features []Feature
+	Exits    Exits
+	Features Features
 	Beings   []Being
+}
+
+type Exits struct {
+	North string
+	East  string
+	South string
+	West  string
+}
+
+type Features struct {
+	Feature []Feature
 }
 
 type Feature struct {
--- gen/areas/areas_test.go
+++ gen/areas/areas_test.go
@@ -1,8 +1,10 @@
 package areas
 
 import (
+	"fmt"
 	"gostories/engine/io"
 	console "gostories/engine/io/console"
+	"gostories/things/area"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -25,4 +27,42 @@ func TestLoadFromXMLLoadsBeings(t *testing.T) {
 	assert.Equal(t, 3, len(_areas))
 	catRoom := _areas["cat_room"]
 	assert.Equal(t, 1, len(catRoom.Beings))
+}
+
+func TestLoadFromXMLLoadsFeatures(t *testing.T) {
+	io.ActiveInputOutputHandler = console.NewConsoleInputOutputHandler()
+	_areas := loadFromXML()
+	storeRoom := _areas["store_room"]
+	assert.Equal(t, 1, len(storeRoom.Features))
+	catRoom := _areas["cat_room"]
+	assert.Equal(t, 0, len(catRoom.Features))
+	kitchen := _areas["kitchen"]
+	assert.Equal(t, 1, len(kitchen.Features))
+	println(fmt.Sprintf("%#v", kitchen))
+	fridge := kitchen.Features[0]
+	assert.Equal(t, "fridge", fridge.GetName())
+	assert.Equal(t, "The fridge is empty apart from a tin of sardines.", fridge.GetLookText())
+}
+
+func TestLoadFromXMLLoadsExits(t *testing.T) {
+	io.ActiveInputOutputHandler = console.NewConsoleInputOutputHandler()
+	_areas := loadFromXML()
+	storeRoom := _areas["store_room"]
+	catRoom := _areas["cat_room"]
+	kitchen := _areas["kitchen"]
+
+	// Assert cat room exit
+	assert.Equal(t, catRoom.Exits[area.West].To, storeRoom)
+	assert.Equal(t, catRoom.Exits[area.West].From, catRoom)
+
+	// Assert kitchen exit
+	assert.Equal(t, kitchen.Exits[area.South].To, storeRoom)
+	assert.Equal(t, kitchen.Exits[area.South].From, kitchen)
+
+	// Assert store room exits
+	assert.Equal(t, storeRoom.Exits[area.North].To, kitchen)
+	assert.Equal(t, storeRoom.Exits[area.North].From, storeRoom)
+
+	assert.Equal(t, storeRoom.Exits[area.East].To, catRoom)
+	assert.Equal(t, storeRoom.Exits[area.East].From, storeRoom)
 }
