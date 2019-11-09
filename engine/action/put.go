11eeb180250ed435ef6eb5faa376c60a656dd9c0
JP <dahui58@hotmail.com>
2019-11-09 17:05:56 +0000

    Case with no put trigger in put command

--- engine/action/put.go
+++ engine/action/put.go
@@ -42,10 +42,12 @@ func ExecutePlaceCommand(placeTarget string, placeSecondTarget *string, gameStat
 	// TODO run triggers off certain action interactions
 	if secondTarget != nil {
 		io.ActiveInputOutputHandler.NewLinef("placed %s on %s", actualItem.GetName(), secondTarget.Name)
-		trigger := secondTarget.Triggers["put"]
-		err = logic.EvaluateTrigger(gameState, trigger.Action)
-		if err != nil {
-			io.ActiveInputOutputHandler.NewLinef("trigger error %v", err)
+		trigger, ok := secondTarget.Triggers["put"]
+		if ok {
+			err = logic.EvaluateTrigger(gameState, trigger.Action)
+			if err != nil {
+				io.ActiveInputOutputHandler.NewLinef("trigger error %v", err)
+			}
 		}
 		return secondTarget
 	}
