2df261e00e95624cba586d698008d837d19ce295
JP <dahui58@hotmail.com>
2020-10-17 15:58:51 +0100

    Move all socket logic out of main

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-
-	"github.com/gorilla/websocket"
 
 	"gostories/engine"
 	"gostories/gen/areas"
@@ -18,21 +15,11 @@ func main() {
 	stage.Start(*startRoom)
 }
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
 func initServer() {
 	webSocketReadyChan := make(chan bool, 1)
 	go func() {
 		http.HandleFunc("/sock", func(w http.ResponseWriter, r *http.Request) {
-			conn, err := upgrader.Upgrade(w, r, nil)
-			if err != nil {
-				panic(fmt.Sprintf("failed call to web socket Upgrade: %v", err))
-			}
-			server.Conn = conn
-			server.Start(webSocketReadyChan)
+			server.Start(webSocketReadyChan, w, r)
 		})
 
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
--- server/socket.go
+++ server/socket.go
@@ -2,16 +2,22 @@ package server
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gorilla/websocket"
 )
 
-// Conn provides access to the web socket.
-var Conn *websocket.Conn
+// conn provides access to the web socket.
+var conn *websocket.Conn
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 // Write writes the provided string to the web socket.
 func Write(msg string) {
-	err := Conn.WriteMessage(websocket.TextMessage, []byte(msg))
+	err := conn.WriteMessage(websocket.TextMessage, []byte(msg))
 	if err != nil {
 		// TODO handle errors or return
 		panic(err)
@@ -40,13 +46,22 @@ type chanMessage struct {
 // Start performs the required work to intitialise the buffered channel used for reading messages from the
 // web socket from the game engine, in a blocking manner. It takes bool channel it uses to indicate when
 // it is ready for the game engine to start (once the channels are set up)
-func Start(done chan bool) {
+func Start(done chan bool, w http.ResponseWriter, r *http.Request) {
+	initWebSocket(w, r)
 	initChans()
 	done <- true
 	readForever()
 }
 
-// InitChans sets up the buffered channel used to recieve messages from the web socket and block on reads.
+func initWebSocket(w http.ResponseWriter, r *http.Request) {
+	var err error
+	conn, err = upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		panic(fmt.Sprintf("failed call to web socket Upgrade: %v", err))
+	}
+}
+
+// initChans sets up the buffered channel used to recieve messages from the web socket and block on reads.
 func initChans() {
 	in, out = setUpBufferedChannel()
 }
@@ -89,7 +104,7 @@ func setUpBufferedChannel() (chan<- interface{}, <-chan interface{}) {
 func readForever() {
 	for {
 		// Read message from browser
-		_, msg, err := Conn.ReadMessage()
+		_, msg, err := conn.ReadMessage()
 		if err != nil {
 			fmt.Printf("failed to Read Message: %v", err)
 			return
